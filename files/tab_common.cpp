70f13c68bff904078163171438743fb313ca2a9f
panqiying <1287928950@qq.com>
2020-04-26 23:11:40 +0800

    add tab_common file

---
+++ files/tab_common.cpp
@@ -0,0 +1,60 @@
+﻿#include "xcore_str_util.h"
+#include "tab_common.h"
+
+namespace db 
+{
+
+TabCommon::TabCommon()
+{
+	//empty
+}
+bool TabCommon::CreateTable(string& strSql)
+{
+	string strSqlUtf8;
+	VERIFY(gbk_to_utf8(strSqlUtf8, strSql));
+	if ( strSqlUtf8.length() == 0 ) 
+		return false;
+
+	shared_ptr<Conn> ptrConn = Database::Instance()->Connect();
+	shared_ptr<Result> ptrResult = ptrConn->Prepare(strSqlUtf8.c_str())->Exec();
+	if (NULL == ptrResult.get())
+		return false;
+
+	return true;
+}
+shared_ptr<Conn> TabCommon::Connect()
+{
+	shared_ptr<Conn> ptrConn = Database::Instance()->Connect();
+	return ptrConn;
+}
+bool TabCommon::Delete(const string& table,const string& key)
+{
+	shared_ptr<Conn> ptrConn = Database::Instance()->Connect();
+	if (NULL == ptrConn.get())
+		return false;
+
+	shared_ptr<Result> ptrResult = ptrConn->Prepare("delete from @p_table where `key` = @p_key")
+			->SetParameter("@p_table", table)
+			->SetParameter("@p_key", key)
+			->Exec();
+		if (NULL == ptrResult.get())
+			return false;
+
+	return true;
+}
+bool TabCommon::Delete(const string& table)
+{
+	shared_ptr<Conn> ptrConn = Database::Instance()->Connect();
+	if (NULL == ptrConn.get())
+		return false;
+
+	shared_ptr<Result> ptrResult = ptrConn->Prepare("delete from @p_table ")
+			->SetParameter("@p_table", table)
+			->Exec();
+	if (NULL == ptrResult.get())
+		return false;
+
+	return true;
+}
+
+};//namespace db
---
+++ files/tab_common.h
@@ -0,0 +1,26 @@
+﻿#ifndef _TAB_COMMON_H_
+#define _TAB_COMMON_H_
+
+#include "xcore_define.h"
+
+namespace db 
+{
+
+class TabCommon
+{
+public:
+	TabCommon();
+
+	static bool CreateTable(string& strSql);
+	
+	static shared_ptr<Conn> Connect();
+	
+	static bool Delete(const string& table,const string& key);
+	
+	static bool Delete(const string& table);
+
+};//class
+
+};//namespace db
+using namespace db;
+#endif //_TAB_COMMON_H_
